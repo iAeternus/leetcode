72939cea475101a69ec2a6bfc3bf1f466928c82a
Ricky <w_ziwei2004@163.com>
2026-02-14 11:40:19 +0800

    1219

---
+++ src/backtracing/1219_path_with_maximum_gold.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+class Solution {
+private:
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+
+    bool is_valid(int n, int m, int x, int y) {
+        return x >= 0 && x < n && y >= 0 && y < m;
+    }
+
+public:
+    int getMaximumGold(std::vector<std::vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int max_sum = 0;
+
+        int total_sum = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                total_sum += grid[i][j];
+            }
+        }
+
+        auto dfs = [&](this auto&& dfs, int x, int y, int sum) -> void {
+            sum += grid[x][y];
+            max_sum = std::max(max_sum, sum);
+            if (max_sum == total_sum) {
+                return; // 以达到最大，剪枝
+            }
+
+            int gold = grid[x][y];
+            grid[x][y] = 0;
+            for (int i = 0; i < 4; ++i) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (is_valid(n, m, nx, ny) && grid[nx][ny] > 0) {
+                    dfs(nx, ny, sum);
+                }
+            }
+            grid[x][y] = gold;
+        };
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] != 0) {
+                    dfs(i, j, 0);
+                }
+            }
+        }
+        return max_sum;
+    }
+};
+
+int main() {
+    return 0;
+}
