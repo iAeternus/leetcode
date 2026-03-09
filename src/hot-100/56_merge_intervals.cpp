9735ac9e8d207820db1ee5d2b310833a1e2c0ef7
Ricky <w_ziwei2004@163.com>
2026-03-09 15:04:08 +0800

    56

---
+++ src/hot-100/56_merge_intervals.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> merge(std::vector<std::vector<int>>& intervals) {
+        std::vector<std::vector<int>> ans;
+        if(intervals.empty()) return ans;
+        std::sort(intervals.begin(), intervals.end());
+        for(const auto& interval : intervals) {
+            int start = interval[0], end = interval[1];
+            if(ans.empty() || ans.back()[1] < start) {
+                ans.push_back({start, end});
+            } else {
+                ans.back()[1] = std::max(ans.back()[1], end);
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+
+}
