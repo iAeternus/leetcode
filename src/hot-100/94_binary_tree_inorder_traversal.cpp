0ee95aa164304c9bc03488b89e711e13dbad0011
Ricky <w_ziwei2004@163.com>
2026-03-10 21:00:24 +0800

    94

---
+++ src/hot-100/94_binary_tree_inorder_traversal.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) :
+            val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+    void inorder_traversal(TreeNode* root, std::vector<int>& ans) {
+        if (!root) return;
+        inorder_traversal(root->left, ans);
+        ans.push_back(root->val);
+        inorder_traversal(root->right, ans);
+    }
+
+public:
+    std::vector<int> inorderTraversal(TreeNode* root) {
+        std::vector<int> ans;
+        inorder_traversal(root, ans);
+        return ans;
+    }
+};
+
+int main() {
+}
