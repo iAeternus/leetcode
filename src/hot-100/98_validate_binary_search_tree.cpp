672de8fe574e6ac7dc63dcaafe5ce62250a807fc
Ricky <w_ziwei2004@163.com>
2026-03-11 10:16:49 +0800

    98

---
+++ src/hot-100/98_validate_binary_search_tree.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.cn/problems/validate-binary-search-tree
+#include <bits/stdc++.h>
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) :
+            val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+    bool dfs(TreeNode* node, long min_val, long max_val) {
+        if (!node) return true;
+        if (node->val <= min_val || node->val >= max_val) {
+            return false;
+        }
+        return dfs(node->left, min_val, node->val) && dfs(node->right, node->val, max_val);
+    }
+
+public:
+    bool isValidBST(TreeNode* root) {
+        return dfs(root, LONG_MIN, LONG_MAX);
+    }
+};
+
+int main() {
+}
